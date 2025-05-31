5a0cc71a72d22ecc51116d88bc26945847dc4c69
Mohammad <mopharaz@gmail.com>
2025-06-01 00:53:45 +0330

    fix: GameViewSet Query changed to all Game object

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -8,7 +8,7 @@ class IsGameAdmin(BasePermission):
 class IsAdminOrCreatorWhileWaiting(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.user and request.user.role == 'admin':
+        if request.user and request.user.role == 'admin' and obj.status == 1:
             return True
 
-        return obj.creator == request.user
+        return obj.creator == request.user and obj.status == 1
